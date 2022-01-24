335c424879adf0821f2d11467c17a0791de79ff3
Vasek - Tom C <tom.chauveau@epitech.eu>
2022-01-24 15:17:01 +0100

    Add session and redis storage
    Signed-off-by: Vasek - Tom C <tom.chauveau@epitech.eu>

---
+++ backend/src/redis/redis.module.ts
@@ -0,0 +1,17 @@
+import { DynamicModule } from '@nestjs/common';
+import { RedisModule, RedisModuleOptions } from 'nestjs-redis';
+import { IConfig, IRedisConfig } from '../app.config';
+import { ConfigService } from '@nestjs/config';
+
+export const Redis: DynamicModule = RedisModule.forRootAsync({
+	inject: [ConfigService],
+	useFactory: (config: ConfigService<IConfig>): RedisModuleOptions => {
+		const { host, port, pass: password } = config.get<IRedisConfig>('redis');
+
+		return {
+			host,
+			port,
+			password,
+		};
+	},
+});
