c9d0a27773e19ea3c02a76be721494e9024a5223
Vasek - Tom C <tom.chauveau@epitech.eu>
2022-01-25 00:55:40 +0100

    Update linter rules

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
 	parserOptions: {
 		sourceType: 'module',
 	},
-	plugins: ['@typescript-eslint/eslint-plugin'],
+	plugins: [ '@typescript-eslint/eslint-plugin' ],
 	extends: [
 		'plugin:@typescript-eslint/eslint-recommended',
 		'plugin:@typescript-eslint/recommended',
@@ -15,8 +15,28 @@ module.exports = {
 		jest: true,
 	},
 	rules: {
-		'@typescript-eslint/interface-name-prefix': 'off',
-		'@typescript-eslint/explicit-function-return-type': 'off',
-		'@typescript-eslint/no-explicit-any': 'off',
+		'@typescript-eslint/explicit-function-return-type': 'warn',
+		'@typescript-eslint/no-explicit-any': 'warn',
+		'array-bracket-spacing': [ 2, 'always' ],
+		'@typescript-eslint/naming-convention': [
+			'error',
+			{
+				'selector': 'interface',
+				'format': [ 'PascalCase' ],
+				'custom': {
+					'regex': '^I[A-Z]',
+					'match': true,
+				},
+			},
+			{
+				'selector': [ 'function' ],
+				'format': [ 'camelCase' ],
+				'leadingUnderscore': 'forbid',
+			},
+			{
+				'selector': 'variable',
+				'format': [ 'camelCase', 'UPPER_CASE', 'PascalCase' ],
+			},
+		],
 	},
 };
