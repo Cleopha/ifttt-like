39c4022bd73a956644d03f15912199b888d40dfb
Vasek - Tom C <tom.chauveau@epitech.eu>
2022-01-25 01:02:55 +0100

    Add winston logger
    Signed-off-by: Vasek - Tom C <tom.chauveau@epitech.eu>

---
+++ backend/src/logger/logger.module.ts
@@ -0,0 +1,23 @@
+import { DynamicModule } from '@nestjs/common';
+
+import { WinstonModule, utilities as WinstonModuleUtilities } from 'nest-winston';
+import * as winston from 'winston';
+
+// Reduce code by destructuration
+const { transports, format } = winston;
+
+export const Logger: DynamicModule = WinstonModule.forRoot({
+	format: format.combine(
+		format.timestamp(),
+		WinstonModuleUtilities.format.nestLike()
+	),
+	transports: [
+		new transports.Console(),
+		new transports.File({ filename: 'logs/error.log', level: 'error', format: format.json() }),
+		new transports.File({
+			filename: 'logs/all.log', format: format.combine(
+				format.json(),
+				format.uncolorize()),
+		}),
+	]
+});
