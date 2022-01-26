95b73c17e90e0f7ab92432d395349376487747e4
FringhianQuentin <quentin.fringhian@epitech.eu>
2022-01-26 11:46:42 +0100

    feat: drop up sheet

---
+++ frontend/lib/components/home/explore_menu.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class ExploreMenu extends StatelessWidget {
+  const ExploreMenu({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        if (!kIsWeb)
+          Container(
+            margin: const EdgeInsets.only(top: 25, bottom: 10),
+            width: 23,
+            height: 5,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(2),
+              color: Colors.black,
+            ),
+          ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: 100,
+            itemBuilder: (BuildContext context, int index) {
+              return ListTile(title: Text('Item $index'));
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
--- frontend/lib/components/utils/explore_button.dart
+++ frontend/lib/components/utils/explore_button.dart
@@ -1,13 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 
+import 'package:frontend/components/home/explore_menu.dart';
+
 class ExploreButton extends StatelessWidget {
   const ExploreButton({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return TextButton(
-      onPressed: () {},
+      onPressed: () {
+        showModalBottomSheet(
+          context: context,
+          builder: (context) => const ExploreMenu(),
+          isScrollControlled: true,
+          enableDrag: !kIsWeb,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(10),
+              topRight: Radius.circular(10),
+            ),
+          ),
+          backgroundColor: Colors.white,
+          barrierColor: kIsWeb ? Colors.transparent : null,
+          constraints: BoxConstraints(
+            maxHeight: kIsWeb
+                ? MediaQuery.of(context).size.height - 130
+                : MediaQuery.of(context).size.height * 0.95,
+          ),
+        );
+      },
       child: Container(
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(43),
