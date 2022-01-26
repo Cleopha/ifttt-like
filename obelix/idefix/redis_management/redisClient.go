fbbaa1cfe84815d3474c23e32a915b596f091356
Coline <coline.seguret@epitech.eu>
2022-01-26 19:38:14 +0100

    feat: redis client
    - get and set value in redis db
    - add go-redis

---
+++ obelix/idefix/redis_management/redisClient.go
@@ -0,0 +1,45 @@
+package redis_management
+
+import (
+	"context"
+	"fmt"
+	"github.com/go-redis/redis/v8"
+)
+
+type RClient interface {
+	SetKey(string, string) error
+	GetKey(string) (string, error)
+}
+
+type RedisClient struct {
+	ctx context.Context
+	rdb *redis.Client
+}
+
+func (client RedisClient) SetKey(key, value string) error {
+	err := client.rdb.Set(client.ctx, key, value, 0).Err()
+	if err != nil {
+		return fmt.Errorf("failed to set key: %v", err)
+	}
+	return nil
+}
+
+func (client RedisClient) GetKey(key string) (string, error) {
+	value, err := client.rdb.Get(client.ctx, key).Result()
+	if err == redis.Nil {
+		return "", fmt.Errorf("key does not exist: %v", err)
+	} else if err != nil {
+		return "", fmt.Errorf("failed to get key: %v", err)
+	}
+	return value, nil
+}
+
+func NewRedisClient() RedisClient {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	return RedisClient{ctx: context.Background(), rdb: rdb}
+}
