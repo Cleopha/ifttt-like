5d615771924c788fabf333cdbc2ad08ffc585d56
Coline <coline.seguret@epitech.eu>
2022-01-26 19:38:14 +0100

    feat: redis client
    - get and set value in redis db
    - add go-redis

--- obelix/idefix/go.mod
+++ obelix/idefix/go.mod
@@ -2,4 +2,7 @@ module idefix
 
 go 1.16
 
-require github.com/Shopify/sarama v1.31.0 // indirect
+require (
+	github.com/Shopify/sarama v1.31.0 // indirect
+	github.com/go-redis/redis/v8 v8.11.4 // indirect
+)
--- obelix/idefix/main.go
+++ obelix/idefix/main.go
@@ -2,8 +2,26 @@ package main
 
 import (
 	"fmt"
+	"idefix/redis_management"
+	"log"
 )
 
+// Example for use RedisClient
+func example() {
+	rclient := redis_management.NewRedisClient()
+
+	err := rclient.SetKey("1", "that my boy")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	key, err := rclient.GetKey("1")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("key: ", key)
+}
+
 func main() {
-	fmt.Println("Begin idefix")
+	example()
 }
