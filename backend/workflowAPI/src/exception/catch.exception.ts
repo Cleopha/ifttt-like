576c663564a0e01eec546e89b7c6b5b296be09fa
Vasek - Tom C <tom.chauveau@epitech.eu>
2022-02-05 15:39:55 +0100

    Create catch & logger interceptor
    Signed-off-by: Vasek - Tom C <tom.chauveau@epitech.eu>

---
+++ backend/workflowAPI/src/exception/catch.exception.ts
@@ -0,0 +1,33 @@
+import { Catch, ArgumentsHost, Logger, ExceptionFilter, BadRequestException } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
+
+
+@Catch()
+export class RpcExceptionInterceptor implements ExceptionFilter {
+	private logger = new Logger('RpcException');
+
+	catch(exception: RpcException, host: ArgumentsHost): RpcException {
+		if (exception.message.includes('Prisma')) {
+			this.logger.error(exception)
+			return new RpcException(exception);
+		}
+
+		const ctx = host.getArgs()[2] as any;
+		const handler = ctx.call.handler.path;
+
+		// Retrieve parser data
+		if (exception.name == BadRequestException.name) {
+			const parserException = exception as any;
+			if (parserException.response.message) {
+				this.logger.error(`${ handler } - ${ parserException.name }: ${ parserException.response.message }`);
+			}
+			return new RpcException(exception.getError());
+		}
+
+		const data = host.switchToRpc().getData();
+		this.logger.error(`${ handler } with data '${ JSON.stringify(data) }' : ${ exception.getError() }`);
+
+		// Required to parse pipes exception
+		return new RpcException(exception.getError());
+	}
+}
