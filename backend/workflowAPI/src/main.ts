85a7a87fd08cbea32d309a437c44ef5a56ce4699
Vasek - Tom C <tom.chauveau@epitech.eu>
2022-02-05 15:37:43 +0100

    Configure workflow API listening port & host
    Signed-off-by: Vasek - Tom C <tom.chauveau@epitech.eu>

--- backend/workflowAPI/src/main.ts
+++ backend/workflowAPI/src/main.ts
@@ -1,21 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
+import { IApiConfig } from './config/config.data';
+import { Logger } from '@nestjs/common';
 
 
 async function bootstrap(): Promise<void> {
 	const app = await NestFactory.create(AppModule);
+	const config = app.get(ConfigService);
+	const { host, port } = config.get<IApiConfig>('api')
 
 	await app.connectMicroservice<MicroserviceOptions>({
 		transport: Transport.GRPC,
 		options: {
 			package: [ 'area.workflow', 'area.task' ],
 			protoPath: [ __dirname + '/protos/workflow.proto', __dirname + '/protos/task.proto' ],
-			url: '127.0.0.1:6000',
+			url: `${host}:${port}`,
 		},
 	});
 
 	await app.startAllMicroservices();
+	Logger.log(`Workflow gRPC service listening on ${host}:${port}`)
 }
 
 bootstrap();
