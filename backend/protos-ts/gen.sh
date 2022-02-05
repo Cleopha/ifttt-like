b45c8f926076371404bb05776f2435118ad49fbe
Vasek - Tom C <tom.chauveau@epitech.eu>
2022-02-05 15:36:53 +0100

    Update protobuf to generate NestJS compliant definition
    Signed-off-by: Vasek - Tom C <tom.chauveau@epitech.eu>

--- backend/protos-ts/gen.sh
+++ backend/protos-ts/gen.sh
@@ -3,5 +3,5 @@ mkdir -p workflow
 protoc \
   --plugin="./node_modules/.bin/protoc-gen-ts_proto" \
   --ts_proto_out=./workflow workflow.proto task.proto \
-  --ts_proto_opt=outputEncodeMethods=false,outputJsonMethods=false,outputClientImpl=false
+  --ts_proto_opt=nestJs=true,addGrpcMetadata=true,outputEncodeMethods=false,outputJsonMethods=false,outputClientImpl=false
 
