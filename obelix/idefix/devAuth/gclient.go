cd409f44488420e2c869a368bfff1a098181d3f8
Coline <coline.seguret@epitech.eu>
2022-02-12 12:28:18 +0100

    feat: google auth and GCalendar trigger
    - google auth
    - GCalendar 10 min deadline

---
+++ obelix/idefix/devAuth/gclient.go
@@ -0,0 +1,56 @@
+package devAuth
+
+import (
+	"context"
+	"fmt"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"net/http"
+	"os"
+)
+
+var (
+	CLIENT_ID     = ""
+	CLIENT_SECRET = ""
+	REDIRECT_URL  = ""
+)
+
+func init() {
+	CLIENT_ID = os.Getenv("GOOGLE_CLIENT_ID")
+	CLIENT_SECRET = os.Getenv("GOOGLE_CLIENT_SECRET")
+	REDIRECT_URL = os.Getenv("GOOGLE_REDIRECT_URL")
+}
+
+type Client struct {
+	ctx context.Context
+	Clt *http.Client
+}
+
+func (c *Client) GetCalendarList() (*http.Response, error) {
+	get, err := c.Clt.Get("https://www.googleapis.com/calendar/v3/users/me/calendarList")
+	if err != nil {
+		return nil, fmt.Errorf("failed to get calendar list: %v", err)
+	}
+	return get, nil
+}
+
+// New creates a new Google client
+func New(ctx context.Context, scopes []string) (*Client, error) {
+	conf := &oauth2.Config{
+		ClientID:     CLIENT_ID,
+		ClientSecret: CLIENT_SECRET,
+		Endpoint:     google.Endpoint,
+		RedirectURL:  REDIRECT_URL,
+		Scopes:       scopes,
+	}
+
+	tok, err := GetAccessToken(ctx, conf)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get google access token: %w", err)
+	}
+
+	return &Client{
+		ctx: ctx,
+		Clt: conf.Client(ctx, tok),
+	}, nil
+}
--- obelix/idefix/go.mod
+++ obelix/idefix/go.mod
@@ -3,8 +3,12 @@ module idefix
 go 1.16
 
 require (
-	github.com/Shopify/sarama v1.31.0 // indirect
-	github.com/go-redis/redis/v8 v8.11.4 // indirect
-	github.com/imroc/req/v2 v2.1.0 // indirect
-	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8 // indirect
+	github.com/Shopify/sarama v1.31.0
+	github.com/go-redis/redis/v8 v8.11.4
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
+	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8
+	golang.org/x/sys v0.0.0-20220209214540-3681064d5158 // indirect
+	google.golang.org/api v0.68.0
+	google.golang.org/genproto v0.0.0-20220210181026-6fee9acbd336 // indirect
 )
