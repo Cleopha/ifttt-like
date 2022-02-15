1784310bd7f1aba670bf41a0c779502dbc9be613
Coline <coline.seguret@epitech.eu>
2022-02-15 14:39:27 +0100

    fix: blank line request

--- obelix/idefix/trigger/request.go
+++ obelix/idefix/trigger/request.go
@@ -8,7 +8,6 @@ import (
 
 func ReadBody(res *http.Response) ([]byte, error) {
 	body, err := ioutil.ReadAll(res.Body)
-
 	if err != nil {
 		return nil, fmt.Errorf("failed to read body: %w", err)
 	}
