cb446874b442e8544dd59276ce120a1d6101c72d
Coline <coline.seguret@epitech.eu>
2022-02-15 13:51:40 +0100

    fix: typo blank line in gcalendar

--- obelix/idefix/services/google/gcalendar.go
+++ obelix/idefix/services/google/gcalendar.go
@@ -101,7 +101,6 @@ func (gc *GCalendar) GetRedisState(rc *redis.Client, key string) (string, error)
 	state, err := rc.GetKey(key)
 
 	if err == redisv8.Nil {
-
 		err = rc.SetKey(key, string(rune(NO_ACTIVE)))
 		if err != nil {
 			return "", fmt.Errorf("failed to set value in redis: %w", err)
