977e2db25e8b527110025ca68d21cba4cd6cae89
Luca Georges Francois <luca.georges-francois@epitech.eu>
2022-02-23 13:38:52 +0100

    Hotfix refactor naming issue
    Signed-off-by: Luca Georges Francois <luca.georges-francois@epitech.eu>

--- obelix/idefix/services/google/gcalendar.go
+++ obelix/idefix/services/google/gcalendar.go
@@ -108,7 +108,7 @@ func (gc *GCalendar) SendToKafka(kp sarama.SyncProducer, taskID string) error {
 
 	_, _, err = kp.SendMessage(msg)
 	if err != nil {
-		return fmt.Errorf("failed to send message to task: %w", err)
+		return fmt.Errorf("failed to send message to kafka: %w", err)
 	}
 
 	return nil
