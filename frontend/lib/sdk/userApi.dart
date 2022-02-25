18ce5400dc64b3f1f198200baa1db05d2d6b7c45
Valentin De Matos <valentin.de-matos@epitech.eu>
2022-02-25 17:38:46 +0100

    feat: WorkflowAPI in front sdk

--- frontend/lib/sdk/userApi.dart
+++ frontend/lib/sdk/userApi.dart
@@ -1,118 +1,126 @@
-import 'package:http/http.dart' as http;
-// import 'package:user.dart';
-// import 'package:frontend/utils/services.dart';
+import 'package:dio/dio.dart';
+import 'package:dio_cookie_manager/dio_cookie_manager.dart';
+import 'package:cookie_jar/cookie_jar.dart';
 
 class User {
-  String? uid;
-  final String email;
-  final String password;
+  final String uid;
 
-  String? name;
+  String email;
+  String? password;
 
   User({
     required this.email,
-    required this.password,
+    required this.uid,
   });
 }
 
-void main() {
-  UserAPI api = new UserAPI();
 
-  final String mail = "daazzerzeerzdea@gmail.com";
-  final String password = "dazdaazrazr";
+void main() {
+  Dio dio = Dio(new BaseOptions(
+    baseUrl: 'http://localhost:7001/',
+    connectTimeout: 15000,
+    receiveTimeout: 13000,
+    headers: {
+      "Accept": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": "true",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+    },
+  ));
+
+  UserAPI userAPI = new UserAPI(dio: dio);
+  WorkflowAPI workflowAPI = new WorkflowAPI(dio: dio);
+
+  final String mail = "abc@gmail.com";
+  final String password = "dazdzrazr";
 
   // api.register(mail, password).then((void v) {
-  //   print("registered");
-
-  //   api.me().then((User u) {
-  //     print("me-ed");
-  //   });
-
-  //   api.logout().then((void v) {
-  //     print("logged out");
-  //   });
+  //   print("Registered");
   // });
 
-  api.login(mail, password).then((void v) {
-    print("logged in");
+  userAPI.login(mail, password).then((void v) {
+    print("Registered");
 
-    api.me().then((User u) {
-      print("me-ed");
+    userAPI.me().then((User user) {
+      print(user.email);
+
+      userAPI.me().then((User user) {
+        print("me: ${user.email}");
+      });
+
+      // userAPI.logout().then((void v) {
+      //   print("Logged out");
+      // });
+
+      workflowAPI.getWorkflows(user.uid).then((List<Workflow> workflows) {
+        print(workflows);
+      });
+
+      workflowAPI.postWorkflow(user.uid, "My workflow n2").then((Workflow w) {
+        print("workflow posted");
+
+        workflowAPI.getWorkflow(user.uid, w.id).then((Workflow w) {
+          print("workflow: ${w.name}");
+        });
+
+        workflowAPI.deleteWorkflow(user.uid, w.id).then((void v) {
+          print("workflow deleted");
+        });
+
+        workflowAPI.getWorkflows(user.uid).then((List<Workflow> workflows) {
+          workflowAPI.putWorkflow(user.uid, workflows[0].id, "New Name").then((void v) {
+            print("workflow updated");
+          });
+        });
+
+      });
     });
 
-    api.logout().then((void v) {
-      print("logged out");
-    });
   });
-
-
 }
 
 class UserAPI {
-  final String baseUrl = "http://localhost:7001/";
+  Dio dio;
+
+  UserAPI({
+    required this.dio
+  }) {
+    dio.interceptors.add(CookieManager(CookieJar()));
+  }
+
   final String loginUrl = "auth/login";
   final String registerUrl = "auth/register";
   final String logoutUrl = "auth/logout";
-  final String patchUrl = "user/patch";
-
-  final http.Client client = new http.Client();
+  final String patchUrl = "user/";
+  final String meUrl = "user/me";
 
   Future<void> register(String email, String password) async {
-    String url = baseUrl + registerUrl;
-
-    Map<String, String> body = {
-      "email": email,
-      "password": password
-    };
-
-    Map<String, String> headers = {
-      "Accept": "application/json",
-      "credential": "true",
-      "Access-Control-Allow-Credentials": "true",
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
-    };
-
-
     try {
-      var response = await client.post(Uri.parse(url), headers: headers, body: body);
 
-      if (response.statusCode == 201) {
-        return; // TODO: return user
-      } else {
-        throw Exception("User registration failed with status code ${response.statusCode} and body ${response.body}");
+      Response response = await dio.post(registerUrl, data: {
+        "email": email,
+        "password": password,
+      });
+
+      if (response.statusCode != 201) {
+        throw Exception("Error registering");
       }
+  
     } catch (e) {
       throw e;
     }
   }
 
   Future<void> login(String email, String password) async {
-    String url = baseUrl + loginUrl;
-
-    Map<String, String> body = {
-      "email": email,
-      "password": password,
-    };
-
-    Map<String, String> headers = {
-      "Accept": "application/json",
-      "Access-Control-Allow-Credentials": "true",
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
-    };
-
     try {
-      var response = await client.post(Uri.parse(url), headers: headers, body: body);
 
-      print(response);
-      // print(response.cookies);
+      Response response = await dio.post(loginUrl, data: {
+        "email": email,
+        "password": password,
+      });
 
-      if (response.statusCode == 200) {
-        // TODO: fill User class by calling /me endpoint
-        return; // TODO: return user
-      } else {
-        throw Exception("User login failed with status code ${response.statusCode} and body ${response.body}");
+      if (response.statusCode != 200) {
+        throw Exception("Error logging in");
       }
     } catch (e) {
       throw e;
@@ -121,13 +129,26 @@ class UserAPI {
 
   Future<void> logout() async {
     try {
-      var response = await client.post(Uri.parse(baseUrl + logoutUrl));
+      Response response = await dio.post(logoutUrl);
 
-      if (response.statusCode == 200) {
-        return;
+      if (response.statusCode != 200) {
+        throw Exception("Error logging out");
+      }
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  Future<User> me() async {
+    try {
+
+      Response response = await dio.get(meUrl);
+
+      if (response.statusCode != 200) {
+        throw Exception("Error getting user");
       }
 
-      throw Exception("User logout failed with status code ${response.statusCode} and body ${response.body}");
+      return User(email: response.data['email'], uid: response.data['id']);
 
     } catch (e) {
       throw e;
@@ -135,54 +156,144 @@ class UserAPI {
   }
 
   Future<void> patch(User user) async {
-    // String url = baseUrl + patchUrl + "/" + user.uid;
-
-    // Map<String, String> body = {
-    //   "name": user.name,
-    //   "email": user.email,
-    //   "password": user.password,
-    // };
-
-    // Map<String, String> headers = {
-    //   "Accept": "application/json",
-    //   "credential": "true",
-    // "Access-Control-Allow-Credentials"
-    // };
-
-
-    // try {
-    //   var response = await client.patch(Uri.parse(url), headers: headers, body: body);
-
-    //   if (response.statusCode == 200) {
-    //     return;
-    //   }
-
-    //   throw Exception("User patch failed with status code ${response.statusCode} and body ${response.body}");
-
-    // } catch (e) {
-    //   throw e;
-    // }
-  }
-
-  Future<User> me() async {
-    String url = baseUrl + "user/me";
-
-    Map<String, String> headers = {
-      "Accept": "application/json",
-      "Access-Control-Allow-Credentials": "true",
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
-    };
-
     try {
-      var response = await client.get(Uri.parse(url), headers: headers);
 
-      if (response.statusCode == 200) {
-        // return User.fromJson(json.decode(response.body));
-        return User(email: "lol", password: "lol");
+      Response response = await dio.patch(patchUrl + user.uid, data: {
+        "email": user.email,
+      });
+
+      if (response.statusCode != 200) {
+        throw Exception("Error patching user");
       }
 
-      throw Exception("User me failed with status code ${response.statusCode} and body ${response.body}");
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  Future<void> delete(User user) async {
+    try {
+
+      Response response = await dio.delete(patchUrl + user.uid);
+
+      if (response.statusCode != 200) {
+        throw Exception("Error deleting user");
+      }
+
+    } catch (e) {
+      throw e;
+    }
+  }
+}
+
+class Workflow {
+  final String id;
+  final String name;
+
+  Workflow({
+    required this.id,
+    required this.name,
+  });
+}
+
+class WorkflowAPI {
+  Dio dio;
+
+  WorkflowAPI({
+    required this.dio
+  }) {
+    dio.interceptors.add(CookieManager(CookieJar()));
+  }
+
+  String getWorkflowsUrl(String userId) {
+    return "user/${userId}/workflow";
+  }
+
+  String postWorkflowUrl(String userId) {
+    return "user/${userId}/workflow";
+  }
+
+  Future<List<Workflow>> getWorkflows(String userId) async {
+    try {
+      Response response = await dio.get(getWorkflowsUrl(userId));
+
+      if (response.statusCode != 200) {
+        throw Exception("Error getting workflows");
+      }
+
+      return List<Workflow>.from(response.data.map((dynamic w) {
+        return Workflow(
+          id: w['id'],
+          name: w['name'],
+        );
+      }));
+
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  Future<Workflow> getWorkflow(String userId, String workflowId) async {
+    try {
+      Response response = await dio.get(getWorkflowsUrl(userId) + "/${workflowId}");
+
+      if (response.statusCode != 200) {
+        throw Exception("Error getting workflow");
+      }
+
+      return Workflow(
+        id: response.data['id'],
+        name: response.data['name'],
+      );
+
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  Future<Workflow> postWorkflow(String userId, String name) async {
+    try {
+      Response response = await dio.post(postWorkflowUrl(userId), data: {
+        "name": name,
+      });
+
+      if (response.statusCode != 201) {
+        throw Exception("Error posting workflow");
+      }
+
+      return Workflow(
+        id: response.data['id'],
+        name: name,
+      );
+
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  Future<void> deleteWorkflow(String userId, String workflowId) async {
+    try {
+      Response response = await dio.delete(postWorkflowUrl(userId) + "/${workflowId}");
+
+      if (response.statusCode != 200) {
+        throw Exception("Error deleting workflow");
+      }
+
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  Future<void> putWorkflow(String userId, String workflowId, String name) async {
+    try {
+      Response response = await dio.put(postWorkflowUrl(userId) + "/${workflowId}", data: {
+        "name": name,
+      });
+
+      if (response.statusCode != 200) {
+        throw Exception("Error putting workflow");
+      }
+
     } catch (e) {
       throw e;
     }
