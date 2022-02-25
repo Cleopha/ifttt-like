46a691ec952de6f7330465c5cc43bce71c334947
Luca Georges Francois <luca.georges-francois@epitech.eu>
2022-02-25 18:50:07 +0100

    Update todo comment
    Signed-off-by: Luca Georges Francois <luca.georges-francois@epitech.eu>

--- obelix/panoramix/services/scaleway/credential.go
+++ obelix/panoramix/services/scaleway/credential.go
@@ -3,7 +3,7 @@ package scaleway
 import "os"
 
 // GetKeys returns the access and secret keys from a scaleway account using the credential API.
-// TODO: Replace behaviour with
+// TODO: Replace behaviour with credential API call.
 func GetKeys() (string, string) {
 	return os.Getenv("SCALEWAY_ACCESS_KEY"), os.Getenv("SCALEWAY_SECRET_KEY")
 }
