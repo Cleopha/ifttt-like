ab3fad1c3872398406afcee2febc9e0285a77ca2
Valentin De Matos <valentin.de-matos@epitech.eu>
2022-02-26 13:02:02 +0100

    feat: front sdk add Action to Task

--- frontend/lib/sdk/userApi.dart
+++ frontend/lib/sdk/userApi.dart
@@ -1,3 +1,4 @@
+// import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 import 'package:dio_cookie_manager/dio_cookie_manager.dart';
 import 'package:cookie_jar/cookie_jar.dart';
@@ -61,17 +62,30 @@ void main() {
 
         workflowAPI.getWorkflow(user.uid, w.id).then((Workflow w) {
           print("workflow: ${w.name}");
-        });
 
-        workflowAPI.deleteWorkflow(user.uid, w.id).then((void v) {
-          print("workflow deleted");
-        });
+          workflowAPI.getTask(user.uid, w.id).then((Task task) {
+            print("task: ${task.action.name}");
 
-        workflowAPI.getWorkflows(user.uid).then((List<Workflow> workflows) {
-          workflowAPI.putWorkflow(user.uid, workflows[0].id, "New Name").then((void v) {
-            print("workflow updated");
+            workflowAPI.addAction(user.uid, w.id, "Create PR", "GITHUB_NEW_PR_DETECTED", []).then((Task t) {
+              print("action added");
+
+              workflowAPI.deleteWorkflow(user.uid, w.id).then((void v) {
+                print("workflow deleted");
+              });
+
+            });
+
+
+          });
+
+
+          workflowAPI.getWorkflows(user.uid).then((List<Workflow> workflows) {
+            workflowAPI.putWorkflow(user.uid, workflows[0].id, "New Name").then((void v) {
+              print("workflow updated");
+            });
           });
         });
+
 
       });
     });
@@ -196,6 +210,75 @@ class Workflow {
   });
 }
 
+class RawTask {
+  final String id;
+  final String name;
+  final String type;
+  final String action;
+  final List<dynamic> params;
+  final String nextId;
+
+  RawTask({
+    required this.id,
+    required this.name,
+    required this.type,
+    required this.action,
+    required this.params,
+    required this.nextId,
+  });
+}
+
+
+class ServiceInfo {
+  final String name;
+  final String iconPath;
+  // final Color color;
+
+  ServiceInfo({
+    required this.name,
+    required this.iconPath,
+    // required this.color,
+  });
+}
+
+class ActionInfo {
+  final String name;
+  final ServiceInfo service;
+
+  ActionInfo({
+    required this.name,
+    required this.service,
+  });
+}
+
+class ReactionInfo {
+  final String name;
+  final ServiceInfo service;
+
+  ReactionInfo({
+    required this.name,
+    required this.service,
+  });
+}
+
+class Task {
+  final String? title;
+  final String author;
+  final int numberOfUsers;
+  final ActionInfo action;
+  final List<ReactionInfo> reactions;
+  final bool isActive;
+
+  Task({
+    this.title,
+    required this.author,
+    required this.numberOfUsers,
+    required this.action,
+    required this.reactions,
+    required this.isActive,
+  });
+}
+
 class WorkflowAPI {
   Dio dio;
 
@@ -211,6 +294,10 @@ class WorkflowAPI {
 
   String postWorkflowUrl(String userId) {
     return "user/${userId}/workflow";
+  }
+
+  String getTasksUrl(String userId, String workflowId) {
+    return "user/${userId}/workflow/${workflowId}/task";
   }
 
   Future<List<Workflow>> getWorkflows(String userId) async {
@@ -295,6 +382,236 @@ class WorkflowAPI {
       }
 
     } catch (e) {
+      throw e;
+    }
+  }
+
+  Future<Task> getTask(String userId, String workflowId) async {
+    try {
+      Response response = await dio.get(getTasksUrl(userId, workflowId));
+
+      if (response.statusCode != 200) {
+        throw Exception("Error getting tasks");
+      }
+
+      List<RawTask> rawTasks = List<RawTask>.from(response.data.map((dynamic t) {
+        return RawTask(
+          id: t['id'],
+          name: t['name'],
+          type: t['type'],
+          action: t['action'],
+          params: t['params'],
+          nextId: t['nextId'],
+        );
+      }));
+
+      if (rawTasks.length == 0) {
+        return Task(author: userId, numberOfUsers: 1, action: ActionInfo(name: "", service: ServiceInfo(iconPath: "TODO", name: "TODO")), reactions: [], isActive: false);
+      }
+
+      List<RawTask> sortedRawTasks = [];
+      
+      for (RawTask rawTask in rawTasks) {
+        if (rawTask.type == "ACTION") {
+          sortedRawTasks.add(rawTask);
+        }
+      }
+
+      if (sortedRawTasks.length == 0) {
+        throw Exception("No action tasks found");
+      }
+
+      while (sortedRawTasks.length > 0) {
+        RawTask rawTask = sortedRawTasks.firstWhere((RawTask t) => t.id == sortedRawTasks.last.nextId);
+
+        sortedRawTasks.remove(rawTask);
+        sortedRawTasks.add(rawTask);
+      }
+
+      ActionInfo action = ActionInfo(
+        name: sortedRawTasks.first.action,
+        service: ServiceInfo(
+          name: "TODO",
+          iconPath: "TODO",
+        ),
+      );
+      sortedRawTasks.removeAt(0);
+
+      return Task(
+        title: workflowId,
+        action: action,
+        author: userId,
+        numberOfUsers: 1,
+        isActive: true,
+        reactions: List<ReactionInfo>.from(sortedRawTasks.map((RawTask rawTask) {
+          return ReactionInfo(
+            name: rawTask.action,
+            service: ServiceInfo(
+              name: "TODO",
+              iconPath: "TODO",
+            ),
+          );
+        })),
+      );
+
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  Future<Task> addAction(String userId, String workflowId, String title, String action, List<dynamic> params) async {
+    try {
+      Response response = await dio.get(getTasksUrl(userId, workflowId));
+
+      if (response.statusCode != 200) {
+        throw Exception("Error getting tasks");
+      }
+
+      List<RawTask> rawTasks = List<RawTask>.from(response.data.map((dynamic t) {
+        return RawTask(
+          id: t['id'],
+          name: t['name'],
+          type: t['type'],
+          action: t['action'],
+          params: t['params'],
+          nextId: t['nextId'],
+        );
+      }));
+
+      RawTask? actionTask = null;
+      
+      for (RawTask rawTask in rawTasks) {
+        if (rawTask.type == "ACTION") {
+          actionTask = rawTask;
+        }
+      }
+
+      if (actionTask != null) {
+        throw Exception("Action tasks already exist");
+      }
+
+      RawTask? firstReaction = null;
+
+      for (RawTask t in rawTasks) {
+        bool isNextIdSet = false;
+
+        for (RawTask t2 in rawTasks) {
+          if (t2.nextId == t.id) {
+            isNextIdSet = true;
+            break;
+          }
+        }
+
+        if (!isNextIdSet) {
+          firstReaction = t;
+          break;
+        }
+      }
+
+      if (firstReaction == null && rawTasks.length != 0) {
+        throw Exception("Reactions produce a circular dependency (E2)");
+      } else {
+        print(rawTasks.length);
+        firstReaction = actionTask;
+      }
+
+      String nextId = "";
+
+      if (firstReaction != null) {
+        nextId = firstReaction.id;
+      }
+
+      print({
+        "name": title,
+        "type": "ACTION",
+        "action": action,
+        "params": {}, // TODO: params
+        "nextId": nextId,
+      });
+
+      response = await dio.post("/user/$userId/workflow/$workflowId/task", data: {
+        "name": title,
+        "type": "ACTION",
+        "action": action,
+        "params": {}, // TODO: params
+        "nextId": nextId,
+      });
+
+      if (response.statusCode != 201) {
+        throw Exception("Error posting task");
+      }
+
+      return getTask(userId, workflowId);
+
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  Future<Task> addReaction(String userId, String workflowId, String title, String reaction, List<dynamic> params) async {
+    try {
+      Response response = await dio.get(getTasksUrl(userId, workflowId));
+
+      if (response.statusCode != 200) {
+        throw Exception("Error getting tasks");
+      }
+
+      List<RawTask> rawTasks = List<RawTask>.from(response.data.map((dynamic t) {
+        return RawTask(
+          id: t['id'],
+          name: t['name'],
+          type: t['type'],
+          action: t['action'],
+          params: t['params'],
+          nextId: t['nextId'],
+        );
+      }));
+
+      RawTask? lastReaction = null;
+
+      for (RawTask t in rawTasks) {
+        if (t.nextId.length == 0) {
+          lastReaction = t;
+        }
+      }
+
+      if (lastReaction == null) {
+        throw Exception("Reactions produce a circular dependency (E1)");
+      }
+
+      response = await dio.post("/user/$userId/workflow/$workflowId/task", data: {
+        "name": title,
+        "type": "REACTION",
+        "action": reaction,
+        "params": params,
+        "nextId": "",
+      });
+
+      if (response.statusCode != 201) {
+        throw Exception("Error posting task");
+      }
+
+      RawTask rawTask = RawTask(
+        id: response.data['id'],
+        name: title,
+        type: "REACTION",
+        action: reaction,
+        params: params,
+        nextId: "",
+      );
+
+      response = await dio.patch("/user/$userId/workflow/$workflowId/task/${lastReaction.id}", data: {
+        "nextId": rawTask.id,
+      });
+
+      if (response.statusCode != 200) {
+        throw Exception("Error patching task");
+      }
+
+      return getTask(userId, workflowId);
+
+    } catch (e) {
+      // TODO: PATCH the lastReaction to have the nextId of the new reaction (API)
       throw e;
     }
   }
