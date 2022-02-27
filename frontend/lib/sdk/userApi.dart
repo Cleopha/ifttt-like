1d09c8a3bf7094d2177265bb6caf724fae26f4fb
Valentin De Matos <valentin.de-matos@epitech.eu>
2022-02-27 20:13:28 +0100

    feat: front sdk now handle credentials

--- frontend/lib/sdk/userApi.dart
+++ frontend/lib/sdk/userApi.dart
@@ -791,3 +791,142 @@ class WorkflowAPI {
     }
   }
 }
+
+class CredentialApi {
+  Dio dio;
+
+  CredentialApi({
+    required this.dio
+  }) {
+    dio.interceptors.add(CookieManager(CookieJar()));
+  }
+
+  Future<List<Map<String, String>>> getStorage(String userId) async {
+    try {
+      Response response = await dio.get("/user/$userId/storage");
+
+      if (response.statusCode != 200) {
+        throw Exception("Error getting storage");
+      }
+
+      return List<Map<String, String>>.from(response.data["credentials"]);
+
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  Future<void> createStorage(String userId) async {
+    try {
+      Response response = await dio.post("/user/$userId/storage");
+
+      if (response.statusCode != 201) {
+        throw Exception("Error creating storage");
+      }
+
+      return;
+
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  Future<void> deleteStorage(String userId) async {
+    try {
+      Response response = await dio.delete("/user/$userId/storage");
+
+      if (response.statusCode != 200) {
+        throw Exception("Error deleting storage");
+      }
+
+      return;
+
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  Future<String> createCredential(String userId, String service, String token) async {
+    if (service.length == 0 || ["GITHUB", "GOOGLE", "SCALEWAY", "COINMARKET", "DOCKER", "ONEDRIVE", "NOTION"]
+        .contains(service) == false) {
+      throw Exception("Invalid credential");
+    }
+
+    try {
+      Response response = await dio.post("/user/$userId/storage/credential?service=$service", data: {
+        "token": token,
+      });
+
+      if (response.statusCode != 201) {
+        throw Exception("Error creating credential");
+      }
+
+      return response.data['token'];
+
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  Future<void> deleteCredential(String userId, String service) async {
+    if (service.length == 0 || ["GITHUB", "GOOGLE", "SCALEWAY", "COINMARKET", "DOCKER", "ONEDRIVE", "NOTION"]
+        .contains(service) == false) {
+      throw Exception("Invalid credential");
+    }
+
+    try {
+      Response response = await dio.delete("/user/$userId/storage/credential?service=$service");
+
+      if (response.statusCode != 200) {
+        throw Exception("Error deleting credential");
+      }
+
+      return;
+
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  Future<void> putCredential(String userId, String service, String token) async {
+    if (service.length == 0 || ["GITHUB", "GOOGLE", "SCALEWAY", "COINMARKET", "DOCKER", "ONEDRIVE", "NOTION"]
+        .contains(service) == false) {
+      throw Exception("Invalid credential");
+    }
+
+    try {
+      Response response = await dio.put("/user/$userId/storage/credential?service=$service", data: {
+        "token": token,
+      });
+
+      if (response.statusCode != 200) {
+        throw Exception("Error patching credential");
+      }
+
+      return;
+
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  Future<String> getCredential(String userId, String service) async {
+    if (service.length == 0 || ["GITHUB", "GOOGLE", "SCALEWAY", "COINMARKET", "DOCKER", "ONEDRIVE", "NOTION"]
+        .contains(service) == false) {
+      throw Exception("Invalid credential");
+    }
+
+    try {
+      Response response = await dio.get("/user/$userId/storage/credential?service=$service");
+
+      if (response.statusCode != 200) {
+        throw Exception("Error getting credential");
+      }
+
+      return response.data['token'];
+
+    } catch (e) {
+      throw e;
+    }
+  }
+}
