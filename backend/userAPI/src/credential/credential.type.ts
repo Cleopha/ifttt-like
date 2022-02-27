80a924b51cb6d781432edcf6212c8372832e9d93
Luca Georges Francois <luca.georges-francois@epitech.eu>
2022-02-27 12:22:29 +0100

    Update credential API migration and add signature verification in idefix
    Signed-off-by: Luca Georges Francois <luca.georges-francois@epitech.eu>

--- backend/userAPI/src/credential/credential.type.ts
+++ backend/userAPI/src/credential/credential.type.ts
@@ -1,11 +1,12 @@
 export const TsService =  {
-	GITHUB: "GITHUB",
+	UNKNOWN: "UNKNOWN",
 	GOOGLE: "GOOGLE",
 	SCALEWAY: "SCALEWAY",
 	COINMARKET: "COINMARKET",
 	DOCKER: "DOCKER",
 	ONEDRIVE: "ONEDRIVE",
 	NOTION: "NOTION",
+	GITHUB: "GITHUB",
 }
 
 export type TsService = (typeof TsService)[keyof typeof TsService]
