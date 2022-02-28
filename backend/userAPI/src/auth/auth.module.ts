2ecf08d4df9e98e344d7e3203657ba079c606719
Vasek - Tom C <tom.chauveau@epitech.eu>
2022-02-28 01:10:04 +0100

    Refactor NestJS architecture to use credentialAPIStorageClient in OAuthModule
    Signed-off-by: Vasek - Tom C <tom.chauveau@epitech.eu>

--- backend/userAPI/src/auth/auth.module.ts
+++ backend/userAPI/src/auth/auth.module.ts
@@ -1,16 +1,12 @@
 import { forwardRef, Module } from '@nestjs/common';
 
-import { UserModule } from '@user';
 
 import { AuthController } from './auth.controller';
-import { AuthGuard } from './auth.guard';
-import { OwnerGuard } from './owner.guard';
+import { UserModule } from '@user';
 
 @Module({
 	imports: [ forwardRef(() => UserModule) ],
 	controllers: [ AuthController ],
-	providers: [ AuthGuard, OwnerGuard ],
-	exports: [ AuthGuard ]
 })
 export class AuthModule {
 }
