0576318a819ad6fcd5ce7d10c2cec4d31f185e96
FringhianQuentin <quentin.fringhian@epitech.eu>
2022-03-02 18:08:39 +0100

    rework: service list

--- frontend/lib/utils/services.dart
+++ frontend/lib/utils/services.dart
@@ -42,26 +42,26 @@ Map<String, ServiceInfo> services = {
     iconPath: 'assets/icons/tags/github.svg',
     color: const Color(0xff4078c0),
   ),
-  'spotify': ServiceInfo(
-    name: 'Spotify',
-    description:
-        'Spotify is a digital music service that gives you access to millions of songs. Applets can help you save your Discover Weekly and Release Radar playlists, share your favorite tunes, and much more.',
-    iconPath: 'assets/icons/tags/spotify.svg',
-    color: const Color(0xff1db954),
-  ),
   'google': ServiceInfo(
-    name: 'Google Drive',
+    name: 'Google',
     description:
-        'Google Drive lets you store and access your files anywhere — on the web, on your hard drive, or on the go. Applets let you send the most important information into your Google Drive, automatically',
-    iconPath: 'assets/icons/tags/googleDrive.svg',
+        'Google est un moteur de recherche gratuit et libre d\'accès sur le World Wide Web, ayant donné son nom à la société Google. C\'est aujourd\'hui le moteur de recherche et le site web le plus visité au monde3 : 90 % des internautes l\'utilisaient en 2018.',
+    iconPath: 'assets/icons/tags/google.svg',
     color: const Color(0xff23448b),
   ),
-  'slack': ServiceInfo(
-    name: 'Slack',
+  'coinmarketcap': ServiceInfo(
+    name: 'CoinMarketCap',
     description:
-        'Slack brings all your team communication into one place. Turn on Applets to bring important information into channels, quickly share photos, automate reminders, and much more.',
-    iconPath: 'assets/icons/tags/slack.svg',
-    color: const Color(0xff4a154b),
+        'CoinMarketCap est le site de référence pour suivre les prix des crypto-actifs dans un monde des cryptomonnaies en pleine expansion. Sa mission est de rendre la crypto plus accessible et efficace au grand public en lui fournissant des informations impartiales, qualitatives et précises afin qu\'ils puissent en tirer leurs propres conclusions.',
+    iconPath: 'assets/icons/tags/coinmarketcap.svg',
+    color: const Color(0xff1db954),
+  ),
+  'nist': ServiceInfo(
+    name: 'Nist',
+    description:
+        'Le National Institute of Standards and Technology, est une agence du département du Commerce des États-Unis. Son but est de promouvoir l\'économie en développant des technologies, la métrologie et des normes de concert avec l\'industrie.',
+    iconPath: 'assets/icons/tags/nist.svg',
+    color: const Color(0xff333333),
   ),
   'scaleway': ServiceInfo(
     name: 'Scaleway',
@@ -69,26 +69,6 @@ Map<String, ServiceInfo> services = {
         'Notre mission est de vous apporter le cloud qui donne et a du sens avec une combinaison de puissance de calcul et stockage conforme aux normes RGPD : flexible, fiable, sécurisée, durable et au juste prix. Avec plus de 20 ans d’expertise dans l’innovation, nous proposons un écosystème complet : du bare metal aux datacenters, en passant par l’intelligence artificielle, le calcul, ou encore des serveurs dédiés haut de gamme. Scaleway est le seul fournisseur de type triple play à proposer à la fois de la colocation en datacenter, du cloud public et du cloud privé, le tout avec une approche innovante et holistique pour une consommation énergétique responsable.',
     iconPath: 'assets/icons/tags/scaleway.svg',
     color: const Color(0xff510099),
-  ),
-  'discord': ServiceInfo(
-    name: 'Discord',
-    description:
-        'Whether you’re part of a school club, gaming group, worldwide art community, or just a handful of friends that want to spend time together, Discord makes it easy to talk every day and hang out more often.',
-    iconPath: 'assets/icons/tags/discord.svg',
-    color: const Color(0xff7289da),
-  ),
-  'notion': ServiceInfo(
-    name: 'Notion',
-    description:
-        'A new tool that blends your everyday work apps into one. It\'s the all-in-one workspace for you and your team.',
-    iconPath: 'assets/icons/tags/notion.svg',
-    color: Colors.black,
-  ),
-  'timer': ServiceInfo(
-    name: 'Timer',
-    description: 'Use Timer services to time your reactions.',
-    iconPath: 'assets/icons/tags/timer.svg',
-    color: const Color(0xff333333),
   ),
 };
 
@@ -119,57 +99,12 @@ Map<String, ActionInfo> actions = {
   ),
   'COINMARKETCAP_ASSET_VARIATION_DETECTED': ActionInfo(
     name: 'Une variation d\'un Asset a été détectée',
-    service: services['scaleway']!,
+    service: services['coinmarketcap']!,
   ),
   'NIST_NEW_CVE_DETECTED': ActionInfo(
     name: 'Une nouvelle CVE a été détectée',
-    service: services['scaleway']!,
+    service: services['nist']!,
   ),
 };
 
-Map<String, ReactionInfo> reactions = {
-  'GOOGLE_CREATE_NEW_EVENT': ReactionInfo(
-    name: 'Création d\'un nouvel Evenement',
-    service: services['google']!,
-  ),
-  'GOOGLE_CREATE_NEW_DOCUMENT': ReactionInfo(
-    name: 'Création d\'un nouveau Document',
-    service: services['google']!,
-  ),
-  'GOOGLE_CREATE_NEW_SHEET': ReactionInfo(
-    name: 'Création d\'un nouveau Sheet',
-    service: services['google']!,
-  ),
-  'SCALEWAY_CREATE_NEW_FLEXIBLE_IP': ReactionInfo(
-    name: 'Création d\'une nouvelle IP Flexible',
-    service: services['scaleway']!,
-  ),
-  'SCALEWAY_CREATE_NEW_INSTANCE': ReactionInfo(
-    name: 'Création d\'une nouvelle Instance',
-    service: services['scaleway']!,
-  ),
-  'SCALEWAY_CREATE_NEW_DATABASE': ReactionInfo(
-    name: 'Création d\'une nouvelle Base de Données',
-    service: services['scaleway']!,
-  ),
-  'SCALEWAY_CREATE_NEW_KUBERNETES_CLUSTER': ReactionInfo(
-    name: 'Création d\'un nouveau Cluster Kubernetes',
-    service: services['scaleway']!,
-  ),
-  'SCALEWAY_CREATE_NEW_CONTAINER_REGISTRY': ReactionInfo(
-    name: 'Création d\'un nouveau Registre de Conteneur',
-    service: services['scaleway']!,
-  ),
-  'ONEDRIVE_CREATE_NEW_POWER_POINT': ReactionInfo(
-    name: 'Création d\'un nouveau Power Point',
-    service: services['google']!,
-  ),
-  'ONEDRIVE_CREATE_NEW_FORM': ReactionInfo(
-    name: 'Création d\'un nouveau Formulaire',
-    service: services['google']!,
-  ),
-  'NOTION_CREATE_NEW_PAGE': ReactionInfo(
-    name: 'Création d\'une nouvelle Page',
-    service: services['notion']!,
-  ),
-};
+Map<String, ReactionInfo> reactions = {};
