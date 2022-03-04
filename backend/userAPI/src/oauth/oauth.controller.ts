a7441dd7112fb0822901fa44e725a73041d5e5fa
Vasek - Tom C <tom.chauveau@epitech.eu>
2022-03-04 22:53:42 +0100

    Implement /oauth/login method to register with any oauth method.
    Signed-off-by: Vasek - Tom C <tom.chauveau@epitech.eu>

--- backend/userAPI/src/oauth/oauth.controller.ts
+++ backend/userAPI/src/oauth/oauth.controller.ts
@@ -1,17 +1,71 @@
-import { Controller, Post, Session } from '@nestjs/common';
+import { Body, Controller, InternalServerErrorException, Post, Session } from '@nestjs/common';
+
 import { CredentialAPIStorageClient } from '@credential/client/credentialAPIStorage.client';
+import { CredentialAPIClient } from '@credential/client/credentialAPI.client';
 import { UserService } from '@user';
 import { ISession } from '@session';
 
+import { LoginDto } from './dto/login.dto';
+import { Convertor } from '@util/convertor';
+import { ApiTags } from '@nestjs/swagger';
+import { hash } from 'bcrypt';
+
+@ApiTags('user')
 @Controller('oauth')
 export class OauthController {
 	constructor(
 		private readonly storageClient: CredentialAPIStorageClient,
+		private readonly credentialClient: CredentialAPIClient,
 		private readonly userService: UserService
-	) {}
+	) {
+	}
 
 	@Post('/login')
-	async login(@Session() session: ISession): Promise<string> {
-		return 'TODO'
+	async login(@Body() loginDto: LoginDto, @Session() session: ISession): Promise<string> {
+		const { email, accessToken, type } = loginDto;
+
+		let user = await this.userService.getByEmail(`${ email }-${ type }`);
+		if (user) {
+			try {
+				await this.credentialClient.updateCredential({
+					owner: user.id,
+					service: Convertor.tsServiceToGrpcService(type),
+					token: accessToken
+				});
+			} catch (e) {
+				throw new InternalServerErrorException(e.msg);
+			}
+
+			session.user = { id: user.id };
+			session.save();
+			return 'User successfully logged in';
+		}
+
+		try {
+			const hashedPassword = await hash(`${ email }-${ type }`, 10);
+			user = await this.userService.create({
+				email: `${ email }-${ type }`,
+				password: hashedPassword,
+			});
+
+			// Create storage
+			await this.storageClient.createStorage({
+				owner: user.id
+			});
+
+			// Add service
+			await this.credentialClient.insertCredential({
+				owner: user.id,
+				service: Convertor.tsServiceToGrpcService(type),
+				token: accessToken
+			});
+
+			session.user = { id: user.id };
+			session.save();
+
+			return 'User successfully logged in';
+		} catch (e) {
+			throw new InternalServerErrorException(e.msg);
+		}
 	}
 }
